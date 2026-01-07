a026bcdc936128b93b11ea79372e2a0e7f2543b9
Bhargav <n190106@rguktn.ac.in>
2026-01-07 17:50:49 +0530

    cleaningservice

--- App.tsx
+++ App.tsx
@@ -1,19 +1,16 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import { NewAppScreen } from '@react-native/new-app-screen';
+import React from 'react';
 import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
 import {
   SafeAreaProvider,
   useSafeAreaInsets,
 } from 'react-native-safe-area-context';
 
+import CleaningServicesScreen from './src/screens/CleaningServicesScreen';
+
+
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
+
 
   return (
     <SafeAreaProvider>
@@ -23,23 +20,24 @@ function App() {
   );
 }
 
+
 function AppContent() {
   const safeAreaInsets = useSafeAreaInsets();
 
+
   return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
+    <View style={[styles.container, { paddingTop: safeAreaInsets.top }]}>
+      <CleaningServicesScreen/>
     </View>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
 });
+
 
 export default App;
